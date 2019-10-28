b420377b4cf36b295015d6d1fa7c2c21eaab0f0e
Buster413 <35501046+Buster413@users.noreply.github.com>
2019-10-27 22:26:09 -0500

    Update script with error checking
    The updated version of the PasswordGenerator script checks the password requirements entered by the user to determine if they are valid.

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,19 +1,70 @@
+# this program takes input from a user on what constraints they want put in place for their password, and generates a random password to fit those constraints
+
+# imports
 import string
 import random
 
-# this program takes input from a user on what constraints they want put in place for their password, and generates a random password to fit those constraints
+#setup
+current_length = 0 #  current length of the password as input by the user's constraints
 
-# get password contstraints from user
-password_length = input("How long do you want your password to be (a good password is at least 8 characters long)?\n")
-num_uppercase = input("How many uppercase letters do you want your password to have?\n")
-num_numbers = input("How many numbers do you want your password to have?\n")
-num_symbols = input("How many symbols do you want your password to have?\n")
+# get password length and check to make sure it is valid
+while True:
+    password_length = input("How long do you want your password to be (a good password is at least 8 characters long)?\n")
+    try:
+        val = int(password_length)
+    except ValueError:
+        print("\nThe password length must be an integer.\n")
+    if password_length < 0:
+        print("The password length cannot be less than zero.\n")
+        continue
+    break
+current_length = password_length
+
+# get number of uppercase letters in the password, and check to make sure it is valid
+while True:
+    num_uppercase = input("How many uppercase letters do you want your password to have (%s characters remaining)?\n" % current_length)
+    try:
+        val = int(num_uppercase)
+    except ValueError:
+        print("\nThe number of uppercase numbers must be an integer.\n")
+    if num_uppercase > current_length or num_uppercase < 0:
+        print("The chosen number cannot exceed the number of characters remaining in the password, or be less than zero.\n")
+        continue
+    break
+current_length -= num_uppercase
+
+# get number of numbers in the password, and check to make sure it is valid
+while True:
+    num_numbers = input("How many numbers do you want your password to have (%s characters remaining)?\n" % current_length)
+    try:
+        val = int(num_numbers)
+    except ValueError:
+        print("\nThe number of numbers must be an integer.\n")
+    if num_numbers > current_length or num_numbers < 0:
+        print("The chosen number cannot exceed the number of characters remaining in the password, or be less than zero.\n")
+        continue
+    break
+current_length -= num_numbers
+
+# get number of symbols in the password, and check to make sure it is valid
+while True:
+    num_symbols = input("How many symbols do you want your password to have (%s characters remaining)?\n" % current_length)
+    try:
+        val = int(num_symbols)
+    except ValueError:
+        print("\nThe number of numbers must be an integer.\n")
+    if num_symbols > current_length or num_symbols < 0:
+        print("The chosen number cannot exceed the number of characters remaining in the password, or be less than zero.\n")
+        continue
+    break
+current_length -= num_symbols
+
 
 # create password
 password = ''.join([random.choice(string.ascii_uppercase) for n in xrange(num_uppercase)] #  add uppercase letters
                    + [random.choice(string.digits) for n in xrange(num_numbers)] #  add numbers
                    + [random.choice(string.punctuation) for n in xrange(num_symbols)] #  add symbols
-                   + [random.choice(string.ascii_lowercase) for n in xrange(password_length - num_uppercase)]) #  add lowercase letters
+                   + [random.choice(string.ascii_lowercase) for n in xrange(password_length - num_uppercase - num_numbers - num_symbols)]) #  add lowercase letters
 
 # randomize characters in password
 new_pass = ''.join(random.sample(password, len(password)))
